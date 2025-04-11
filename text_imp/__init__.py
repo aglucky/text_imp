084f515aae88c2e2a37b102e48340a08a4694862
Adam Gluck <adam.gluck10@gmail.com>
2025-04-11 12:04:13 -0400

    fix init

--- text_imp/__init__.py
+++ text_imp/__init__.py
@@ -1,14 +1,15 @@
-from ._core import (
+from text_imp import (
     get_attachments,
     get_chat_handles,
     get_chats,
     get_handles,
     get_messages,
 )
+
 from .attachments import get_attachments_with_guid
 from .contacts import get_contacts
 
-# Re-export the functions from _core
+# Re-export the functions from text_imp
 get_attachments = get_attachments
 get_chat_handles = get_chat_handles
 get_chats = get_chats
