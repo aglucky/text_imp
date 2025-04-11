c75f7fd178ec5ae98307989aee16410e31acf71c
Adam Gluck <adam.gluck10@gmail.com>
2025-04-11 12:22:31 -0400

    fix attachments

--- text_imp/__init__.py
+++ text_imp/__init__.py
@@ -1,12 +1,13 @@
-from .attachments import get_attachments_with_guid
-from .contacts import get_contacts
-from .text_imp import (
+from text_imp import (
     get_attachments,
     get_chat_handles,
     get_chats,
     get_handles,
     get_messages,
 )
+
+from .attachments import get_attachments_with_guid
+from .contacts import get_contacts
 
 __all__ = [
     "get_attachments",
