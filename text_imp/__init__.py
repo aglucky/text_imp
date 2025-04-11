9b0a52f5fd68434bbe7dd623e7778c6c7a1e1890
Adam Gluck <adam.gluck10@gmail.com>
2025-04-11 12:27:54 -0400

    add macos feature

--- text_imp/__init__.py
+++ text_imp/__init__.py
@@ -1,13 +1,12 @@
-from text_imp import (
+from .attachments import get_attachments_with_guid
+from .contacts import get_contacts
+from .text_imp import (
     get_attachments,
     get_chat_handles,
     get_chats,
     get_handles,
     get_messages,
 )
-
-from .attachments import get_attachments_with_guid
-from .contacts import get_contacts
 
 __all__ = [
     "get_attachments",
